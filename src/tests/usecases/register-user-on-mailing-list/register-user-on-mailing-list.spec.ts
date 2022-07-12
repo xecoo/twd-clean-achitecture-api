2c31f01c2af8c7fa162dc2735512eab230b6dd14
Bruno <bruno.pacheco@tusta.com.br>
2022-07-12 18:23:36 -0300

    feat: user data interface

---
+++ src/tests/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,15 @@
+import { UserData } from "../../../usecases/register-user-on-mailing-list/user-data"
+
+describe('Register user on mailing list use case', () => {
+  test('Should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(users)
+    // const usecase: RegisteruserOnMailingList = new RegisterUserOnMailingList(repo)
+    // const name = 'any_name'
+    // const email = 'any@email.com'
+    // const response = await usecase.RegisterUserOnMailingList({ name, email })
+    // const user = repo.findUserByEmail('any@email.com')
+    // expect((await user).name).toBe('any_name')
+  })
+})
