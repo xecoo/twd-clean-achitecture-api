c32f5e81fa598a05875d68b4b02765d322a973ef
Bruno <bruno.pacheco@tusta.com.br>
2022-07-13 09:14:53 -0300

    test: add and find user and checks its  existence

--- src/tests/repository/in-memory-user-repository.spec.ts
+++ src/tests/repository/in-memory-user-repository.spec.ts
@@ -8,4 +8,14 @@ describe('In Memory User repository', () => {
     const user = await userRepo.findUserByEmail('any@email.com')
     expect(user).toBeNull()
   })
+
+  test('should return user if it is found in the repository', async () => {
+    const users: UserData[] = []
+    const name = 'any_name'
+    const email = 'any@email.com'
+    const userRepo = new InMemoryUserRepository(users)
+    await userRepo.add({ name, email })
+    const user = await userRepo.findUserByEmail('any@email.com')
+    expect(user.name).toBe('any_name')
+  })
 })
