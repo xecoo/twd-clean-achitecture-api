de9f9998a1fc617fa36414763e719ebb5c4bf9d7
Bruno <bruno.pacheco@tusta.com.br>
2022-07-13 09:01:58 -0300

    feat: user repository interface

---
+++ src/usecases/ports/user-repository.ts
@@ -0,0 +1,8 @@
+import { UserData } from '../register-user-on-mailing-list/user-data'
+
+export interface UserRepository {
+  add(user: UserData): Promise<void>
+  findUserByEmail(email: string): Promise<UserData>
+  findAllUsers (): Promise<UserData[]>
+  exists(user: UserData): Promise<boolean>
+}
