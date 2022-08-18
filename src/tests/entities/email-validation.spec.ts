aaf724cac56dacd22fad3e93f78dcccb42f45225
Bruno <bruno.pacheco@tusta.com.br>
2022-08-18 16:54:03 -0300

    test: validate emails with empty local parts

--- src/tests/entities/email-validation.spec.ts
+++ src/tests/entities/email-validation.spec.ts
@@ -30,4 +30,9 @@ describe('Email validation', () => {
     const email: string = 'l'.repeat(65) + '@email.com'
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept empty local part ', () => {
+    const email: string = '@email.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
