1e4200ce8c136096e8588aec96dff737e2d8726d
Bruno <bruno.pacheco@tusta.com.br>
2022-08-18 16:56:13 -0300

    feat: validate emails with empty domain parts

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -8,7 +8,7 @@ export class Email {
 
     if (local.length > 64 || local.length === 0) return false
 
-    if (domain.length > 255) return false
+    if (domain.length > 255 || domain.length === 0) return false
 
     return true
   }
