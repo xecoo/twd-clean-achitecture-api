7252c928158520da13b6bb41a1c1f81e95e3c2f4
Bruno <bruno.pacheco@tusta.com.br>
2022-08-18 16:53:54 -0300

    feat: validate emails with empty local parts

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -6,7 +6,7 @@ export class Email {
 
     const [local, domain] = email.split('@')
 
-    if (local.length > 64) return false
+    if (local.length > 64 || local.length === 0) return false
 
     if (domain.length > 255) return false
 
