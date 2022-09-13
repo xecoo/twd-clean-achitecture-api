a47921841ecddea2519431b772ec15415143b645
Bruno <bruno.pacheco@tusta.com.br>
2022-09-13 09:06:13 -0300

    feat: format from typescripts sintax

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,5 +1,5 @@
 export class Email {
-  static validate(email: string): boolean {
+  static validate (email: string): boolean {
     if (!email) return false
 
     if (email.length > 320) return false
