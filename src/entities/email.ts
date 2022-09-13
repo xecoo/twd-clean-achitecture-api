5ee5e48dbbb7e6a0b0f8ea3befa99e4503129cfe
Bruno <bruno.pacheco@tusta.com.br>
2022-09-13 09:00:00 -0300

    feat: validate eail with domain part weith regex

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,8 +1,12 @@
 export class Email {
-  static validate (email: string): boolean {
+  static validate(email: string): boolean {
     if (!email) return false
 
     if (email.length > 320) return false
+
+    const emailRegex = /^[-!#$%'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z])+$/
+
+    if (!emailRegex.test(email)) return false
 
     const [local, domain] = email.split('@')
 
