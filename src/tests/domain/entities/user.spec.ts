5e23512afd374031dd7b2d2ffae17b2673a10288
Bruno <bruno.pacheco@tusta.com.br>
2022-09-14 11:22:29 -0300

    test: verify e-mail laddress when creating user

--- src/tests/domain/entities/user.spec.ts
+++ src/tests/domain/entities/user.spec.ts
@@ -1,6 +1,11 @@
+import { left } from '../../../crossCutting/either'
+import { User } from '../../../domain/entities/user'
+import { InvalidEmailError } from '../../../domain/errors/invalid-email-error'
+
 describe('User domain entity', () => {
   test('should not create user with invalid e-mail address', () => {
-    // const invalidEmail = 'invalid_email'
-    // const error = User.create({ name: 'any_name', email: invalidEmail })
+    const invalidEmail = 'invalid_email'
+    const error = User.create({ name: 'any_name', email: invalidEmail })
+    expect(error).toEqual(left(new InvalidEmailError()))
   })
 })
