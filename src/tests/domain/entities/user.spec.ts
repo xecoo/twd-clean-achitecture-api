76898775e45a6b2493ff13377780249998ea5d8f
Bruno <bruno.pacheco@tusta.com.br>
2022-09-16 08:42:15 -0300

    test: create valid user

--- src/tests/domain/entities/user.spec.ts
+++ src/tests/domain/entities/user.spec.ts
@@ -23,4 +23,13 @@ describe('User domain entity', () => {
 
     expect(error).toEqual(left(new InvalidNameError()))
   })
+
+  test('should not create user with valid date', () => {
+    const validName = 'any_name'
+    const validEmail = 'any@email.com'
+    const user: User = User.create({ name: validName, email: validEmail }).value as User
+
+    expect(user.name.value).toEqual(validName)
+    expect(user.email.value).toEqual(validEmail)
+  })
 })
