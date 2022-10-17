ae6104cf6cf2cc7f92edcda8ba8dd89d82fb68ad
Bruno <bruno.pacheco@tusta.com.br>
2022-10-17 08:47:33 -0300

    chore: improve jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['<roodDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<roodDir>/src/**/*.ts',
+    '!**/test/**',
+    '!**/config/**'
+  ],
   testEnvironment: 'node',
   transform: { '.+\\.ts$': 'ts-jest' }
 }
