0b9dd84fc984065b4de39926b2097278ddb759ec
Bruno <bruno.pacheco@tusta.com.br>
2023-02-24 13:49:26 -0300

    feat: create condition to verify param

--- src/application/web-controllers/ports/register-user-controller.ts
+++ src/application/web-controllers/ports/register-user-controller.ts
@@ -3,6 +3,7 @@ import { UserData } from "@/domain/entities";
 import { badRequest, created } from "@/application/web-controllers/util";
 import { HttpRequest } from "./http-request";
 import { HttpResponse } from "./http-response";
+import { MissingParamError } from "../errors/errors";
 
 export class RegisterUserController{
     private readonly usecase: RegisterUserOnMailingList
@@ -12,6 +13,14 @@ export class RegisterUserController{
     }
 
     public async handle (request: HttpRequest): Promise<HttpResponse> {
+
+        if (!request.body.name || !request.body.email)
+        {
+            let missingParam = !(request.body.name) ? 'name ' : ''
+            missingParam += !(request.body.email) ? 'email' : ''
+            return badRequest(new MissingParamError(missingParam.trim()))
+        }
+            
         const userData: UserData = request.body
         const response = await this.usecase.registerUserOnMailingList(userData)
 
