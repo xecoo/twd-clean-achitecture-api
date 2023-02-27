6c3d87df0cf4a201f7195b2931a28d1cc19cc25c
Bruno <bruno.pacheco@tusta.com.br>
2023-02-27 08:07:43 -0300

    feat: implements interface and update imports

--- src/application/web-controllers/ports/register-user-controller.ts
+++ src/application/web-controllers/ports/register-user-controller.ts
@@ -1,37 +1,39 @@
 import { RegisterUserOnMailingList } from "@/application/usecases/register-user-on-mailing-list";
 import { UserData } from "@/domain/entities";
-import { badRequest, created } from "@/application/web-controllers/util";
-import { HttpRequest } from "./http-request";
-import { HttpResponse } from "./http-response";
-import { MissingParamError } from "../errors/errors";
+import { badRequest, created, serverError } from "@/application/web-controllers/util";
+import { HttpRequest, HttpResponse } from "@/application/web-controllers/ports";
+import { MissingParamError } from "@/application/web-controllers/errors";
+import { UseCase } from "@/application/usecases/ports";
 
-export class RegisterUserController{
+export class RegisterUserController implements UseCase{ 
     private readonly usecase: RegisterUserOnMailingList
 
     constructor(usecase: RegisterUserOnMailingList) {
         this.usecase = usecase
     }
-
-    public async handle (request: HttpRequest): Promise<HttpResponse> {
-
-        if (!request.body.name || !request.body.email)
-        {
-            let missingParam = !(request.body.name) ? 'name ' : ''
-            missingParam += !(request.body.email) ? 'email' : ''
-            return badRequest(new MissingParamError(missingParam.trim()))
-        }
+    public async perform (request: HttpRequest): Promise<HttpResponse> {
+        try {
+            if (!request.body.name || !request.body.email) {
+                let missingParam = !(request.body.name) ? 'name ' : ''
+                missingParam += !(request.body.email) ? 'email' : ''
+                return badRequest(new MissingParamError(missingParam.trim()))
+            }
             
-        const userData: UserData = request.body
-        const response = await this.usecase.registerUserOnMailingList(userData)
+            const userData: UserData = request.body
+            const response = await this.usecase.registerUserOnMailingList(userData)
 
-        if (response.isLeft()) {
-            return badRequest(response.value)
-        }
+            if (response.isLeft()) {
+                return badRequest(response.value)
+            }
 
-        if (response.isRight()) {
-            const httpResponse: HttpResponse = created(response.value)
+            if (response.isRight()) {
+                const httpResponse: HttpResponse = created(response.value)
 
-            return httpResponse
+                return httpResponse
+            }
+        } catch (error)
+        {
+            return serverError(error)
         }
     }
 }
