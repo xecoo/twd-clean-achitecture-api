43db7674d27dc90d9bf807936b153af13f6c15d3
Bruno <bruno.pacheco@tusta.com.br>
2023-03-02 16:45:56 -0300

    test: apply body parsing middleware with json

---
+++ tests/application/main/middleware/body-parser.test.ts
@@ -0,0 +1,16 @@
+import app from '@/application/main/config/app'
+import request from 'supertest'
+
+describe('Body parser middleware', () => {
+    test('should parse body as json', async () => {
+
+        app.post('/test_body_parser', (req, res) => {
+            res.send(req.body)
+        })
+
+        await request(app)
+            .post('/test_body_parser')
+            .send({ name: 'Bruno' })
+            .expect({ name: 'Bruno' })
+    })
+})
