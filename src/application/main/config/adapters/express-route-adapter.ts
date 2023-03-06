202204b79b9d99fdfa0c0f9662d74d6f7468f47c
Bruno <bruno.pacheco@tusta.com.br>
2023-03-06 17:46:05 -0300

    feat: create express-route-adapter

--- src/application/main/config/adapters/express-route-adapter.ts
+++ src/application/main/config/adapters/express-route-adapter.ts
@@ -1,2 +1,14 @@
-import { RegisterUserController } from '@/application/web-controllers/ports/register-user-controller'
+import { RegisterUserController } from '@/application/web-controllers/'
+import { Request, Response } from 'express'
+import {HttpRequest} from '@/application/web-controllers/ports'
 
+export const adaptRoute = (controller: RegisterUserController) => {
+
+    return async (req: Request, res: Response) => {
+        const httpRequest: HttpRequest = {
+            body: req.body
+        }
+        const httpResponse = await controller.perform(httpRequest)
+        res.status(httpResponse.statusCode).json(httpResponse.body)
+    }
+}
