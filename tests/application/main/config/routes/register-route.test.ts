469585e669fd2501ceca0defd1fed0969bcbbedd
Bruno <bruno.pacheco@tusta.com.br>
2023-03-06 18:30:43 -0300

    test: config register route

---
+++ tests/application/main/config/routes/register-route.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+import app from '@/application/main/config/app'
+
+describe('Register routes', () => { 
+    test('should return an account with success',async () => { 
+        app.post('/test_cors', (req, res) => {
+            request.send()
+        })
+        await request(app)
+            .post('/api/register')
+            .send({
+                name: 'Any name',
+                email: 'any@mail.com'
+        })
+        .expect(201)
+    })
+})
