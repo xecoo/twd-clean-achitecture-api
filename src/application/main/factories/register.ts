9b792df219ae0006057932b00f1bba169fad26b0
Bruno <bruno.pacheco@tusta.com.br>
2023-03-06 18:04:06 -0300

    feat: register user controller factory

---
+++ src/application/main/factories/register.ts
@@ -0,0 +1,10 @@
+import { RegisterUserController } from '@/application/web-controllers/'
+import { RegisterUserOnMailingList } from '@/application/usecases/register-user-on-mailing-list/'
+import { InMemoryUserRepository } from '@tests/infra/repository'
+
+export const makeRegisterUserController = (): RegisterUserController => {
+    const inMemoryUserRepository = new InMemoryUserRepository([])
+    const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(inMemoryUserRepository)
+    const registerUserController = new RegisterUserController(registerUserOnMailingListUseCase)
+    return registerUserController
+}
