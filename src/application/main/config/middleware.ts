281b8fc3375e685ae8cbf37d3cb42a6188d16d76
Bruno <bruno.pacheco@tusta.com.br>
2023-03-06 15:31:54 -0300

    feat: apply cors enabling middleware

--- src/application/main/config/middleware.ts
+++ src/application/main/config/middleware.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { bodyParser } from '@/application/main/config/middleware/body-parser'
+import { cors } from '@/application/main/config/middleware/cors'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
