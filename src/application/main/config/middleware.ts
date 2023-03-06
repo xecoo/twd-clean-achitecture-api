1e03b129765ddf40ef7e33e296eeb7e8fc0e413e
Bruno <bruno.pacheco@tusta.com.br>
2023-03-06 15:41:53 -0300

    feat: apply content-type middleware

--- src/application/main/config/middleware.ts
+++ src/application/main/config/middleware.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { bodyParser } from '@/application/main/config/middleware/body-parser'
 import { cors } from '@/application/main/config/middleware/cors'
+import { contentType } from '@/application/main/config/middleware/content-type'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
