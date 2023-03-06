f1f779a8c790805655e983f60c5d93af444ecb21
Bruno <bruno.pacheco@tusta.com.br>
2023-03-06 15:51:34 -0300

    feat: create index for middleware

--- src/application/main/config/middleware.ts
+++ src/application/main/config/middleware.ts
@@ -1,7 +1,5 @@
 import { Express } from 'express'
-import { bodyParser } from '@/application/main/config/middleware/body-parser'
-import { cors } from '@/application/main/config/middleware/cors'
-import { contentType } from '@/application/main/config/middleware/content-type'
+import { bodyParser,contentType, cors } from '@/application/main/config/middleware/'
 
 export default (app: Express): void => {
   app.use(bodyParser)
