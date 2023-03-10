ebbf4d649b11af05b0a95280b252146a74a86aec
Bruno <bruno.pacheco@tusta.com.br>
2023-03-10 09:00:40 -0300

    refactor: change text

--- src/application/main/server.ts
+++ src/application/main/server.ts
@@ -2,5 +2,5 @@ import 'module-alias/register'
 import app from '@/application/main/config/app'
 
 app.listen(5000, () => {
-    console.log('Server run at http://localhost/5000')
+    console.log('Server running at http://localhost/5000')
 })
