7b978d3e0225c2e302549aaa62126453425ff813
Bruno <bruno.pacheco@tusta.com.br>
2023-03-13 11:33:46 -0300

    feat: add user and verify existense

---
+++ src/infra/repository/mongodb/mongodb-user-repository.ts
@@ -0,0 +1,31 @@
+import { UserRepository } from '@/application/usecases/ports'
+import { UserData } from '@/domain/entities'
+import { MongoHelper } from '@/infra/repository/mongodb/helper'
+
+export class MongodbUserRepository implements UserRepository {
+  async add(user: UserData): Promise<void> {
+    const userCollection = MongoHelper.getCollection('users')
+    const exists = await this.exists(user)
+    if (!exists) {
+      await userCollection.insertOne(user)
+    }
+  }
+
+  async findUserByEmail(email: string): Promise<UserData> {
+    const userCollection = MongoHelper.getCollection('users')
+    const result = await userCollection.findOne({ email: email })
+    return result.toArray()
+  }
+
+  findAllUsers(): Promise<UserData[]> {
+    throw new Error('Mehod not implemented. ')
+  }
+
+  async exists(user: UserData): Promise<boolean> {
+    const result = await this.findUserByEmail(user.email)
+    if (result != null) {
+      return true
+    }
+    return false
+  }
+}
